7e16e918099a6d3ba2286df4543ecad4675d7eee
cbradley3351 <christinabradley@live.com>
2020-06-22 14:58:34 -0400

    Splice In/Out

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,20 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
-function spliceElement(someArr, index) {
-  
-}
+
+
+
+function spliceElement(someArray, index)
+    {
+
+    someArray.splice(index-1, 1) //changing to -2, 2 will remove two elements
+    //will remove -1 at one time from the right side of the array
+    colors.splice(5,1, "White","Pink");  //This one SPLICES IN two colors at the 5th postition and it does it 1 time
+    return someArray
+
+    }
+
+let colors = ["Cyan", "Magenta", "Yellow", "Black"]
+
+console.log(spliceElement(colors, 4));
+//Removes the color in the fourth position
