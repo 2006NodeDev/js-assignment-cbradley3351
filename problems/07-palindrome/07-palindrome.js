ce2d94585e1d641f6b60ce79da9590dc2b2e1dc3
cbradley3351 <christinabradley@live.com>
2020-06-22 15:15:21 -0400

    Palindrome

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,12 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
-function isPalindrome(someStr) {
-  
+
+const Palindrome = (str) => {
+    const isPalindrome = str.split('').reverse().join('');  //change string into array to split, array can be reversed, then join array back together with join
+    console.log(str===isPalindrome); //checks to see if string is === to the isPalindrome value that was turned into an array, reversed then joined back together
 }
+
+Palindrome("ooxxoo"); //calls the Palindrome
+Palindrome("luna"); //returns false not a palindrome
+Palindrome("kayak"); //true
