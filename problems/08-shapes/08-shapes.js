5e482e372551f5c0d9fd4befea3b2f63a941f39f
Christina Bradley <66640819+cbradley3351@users.noreply.github.com>
2020-06-22 11:30:01 -0400

    Switch

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -5,21 +5,29 @@ height is a Number and is the height of the shape. Assume the number is odd.
 character is a String that represents the contents of the shape. Assume this String contains just one character.
 Use a switch statement to determine which shape was passed in.
 Use the console.log function to print the desired shape.
-Example for printShape("Square", 3, "%");
-%%%
-%%%
-%%%
-Example for printShape("Triangle", 3, "$");
-$
-$$
-$$$
-Example for printShape("Diamond", 5, "*");
-  *
- ***
-*****
- ***
-  * 
-*/
-function printShape(shape, height, character) {
-  
+
+//Need to come back to this and correct it, but for now it's a switch statement
+
+let printShape = 3;
+
+switch (printShape) {
+  case 3:
+  console.log('Triangle');
+  break;
+
+  case 4:
+  console.log('Square');
+  break;
+
+  case 5:
+  console.log('Pentagon');
+  break;
+
+  case 6:
+  console.log('Hexagon');
+  break;
+
+  default:
+  console.log('No shape stored');
+  break;
 }
