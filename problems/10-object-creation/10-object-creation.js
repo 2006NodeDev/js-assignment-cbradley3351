40ac92fc8296eda986261a5c5a47b3800624440c
Christina Bradley <66640819+cbradley3351@users.noreply.github.com>
2020-06-22 06:39:22 -0400

    object

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,18 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+let dog = {
+  name: 'Luna',
+  age: '2',
+  color: 'white',
+  breed: 'pomeranian',
+  species: 'dog'
+};
+dog.name;
+dog.age;
+dog.color;
+dog.breed;
+dog.species;
+
+console.log(`Hello I'm ${dog.name}.  I am a ${dog.breed} ${dog.species}.  My fur is ${dog.color}.`);
