3c6f1322e28a40029f39e7a5eb1c1d60a97d121a
Christina Bradley <66640819+cbradley3351@users.noreply.github.com>
2020-06-22 04:14:52 -0400

    Reverse String
    Added Reverse String

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -1,6 +1,21 @@
 /* 3. Reverse String
 Define function: reverseStr(someStr)
 Reverse and return the String. */
-function reverseStr(someStr) {
-  
-}
+
+//Reverse the string for example. Hello would be olleH
+//call a reverse function on a array after adding strings indiv. to the array
+//Use the join function to chain the letters back together
+
+
+//The original line
+console.log('Reverse the String!');
+const str = 'Reverse the String!';
+
+
+const newStr = str
+        .split('') //splits the string into letters in an array 
+                    // this allows me to use the reverse function
+        .reverse() //Reverses characters in an array
+        .join(''); //joins the whitespace between the letters
+
+console.log(newStr);
