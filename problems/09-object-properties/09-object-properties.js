78c220fe9b3f598d2f09e2dec48cdb7db7d77b87
Christina Bradley <66640819+cbradley3351@users.noreply.github.com>
2020-06-22 06:52:00 -0400

    Object-Properties

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,6 +1,12 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
-function objectProperties(someObj) {
+function chocolate(brand, flavor, type) {
+    this.brand = brand;
+    this.flavor = flavor;
+    this.type = type;
+  }
 
-}
+  let choco = new chocolate('Nestle', 'Milk', 'Crispy');
+
+  console.log(`${choco.brand}, ${choco.flavor}, ${choco.type}`);
