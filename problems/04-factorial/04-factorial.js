6fc179fb290cd40ad1ea1c7dc5fa9641d1572673
Christina Bradley <66640819+cbradley3351@users.noreply.github.com>
2020-06-22 04:58:03 -0400

    factorial

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -1,6 +1,17 @@
 /* 4. Factorial
 Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
+
+//factorial 5 x 4 x 3 x 2 x 1
+// n! = n x (n-1) x (n-2) x (n-3) x (n-2) x (n-1)
 function factorial(sumNum) {
-  
+    //0! = 1, 1! =1
+        if(sumNum > 0 && sumNum <= 1) {
+        return 1;
+  }
+  else {
+      return sumNum * factorial(sumNum-1);
+  }
 }
+
+console.log(factorial(5));
