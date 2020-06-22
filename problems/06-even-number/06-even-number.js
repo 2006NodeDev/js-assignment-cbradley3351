facda0b39d84745acb9b7099393cef3035292845
cbradley3351 <christinabradley@live.com>
2020-06-22 14:03:46 -0400

    Even Number

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -2,6 +2,26 @@
 Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
-function isEven(someNum) {
-  
+
+
+//0 is even and 1 is odd
+function isEven(someNum){
+    if(someNum<0||someNum>0){
+        num = Math.abs(someNum) //gives the absolute value of the number, non negative value of x reguardless of the sign
+    let f = Math.floor(someNum/2)  // function returns the largest integer less than or equal to a given number.
+    let mod = someNum - 2 * f;
+ 
+    if(mod === 0) {return true}
+    else {return false}
 }
+}
+
+console.log(isEven(41));
+
+
+
+///used mod which is basically % so this probably won't count
+
+
+    
+    
